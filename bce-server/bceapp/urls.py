8139d9723396863932ffc0eec3942c8a650f3171
Michael <michael.musangeya@britecore.com>
2020-02-15 21:05:52 +0200

    Add admin back

--- bce-server/bceapp/urls.py
+++ bce-server/bceapp/urls.py
@@ -1,4 +1,10 @@
 from bce import views
+from django.conf.urls import url
+from django.contrib import admin
 from django.urls import include, path
 
-urlpatterns = [path("api/", include("bce.urls")), path("", views.ApiRoot.as_view(), name="index")]
+urlpatterns = [
+    path("api/", include("bce.urls")),
+    url("admin/", admin.site.urls),
+    path("", views.ApiRoot.as_view(), name="index"),
+]
