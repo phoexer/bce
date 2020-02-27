d620b1940533691c86bb85c28246f7a643203915
Michael Musangeya <phoexer@gmail.com>
2020-02-28 01:29:40 +0200

    Add model and serializer tests
    These were pretty straight forward since there is not a lot of complexity
    This is a design flaw, I'll have to refactor heavily when I finish the
    view tests.

---
+++ bce-server/bce/factories.py
@@ -0,0 +1,42 @@
+import factory
+from bce.models import FieldOption, FieldType, Risk, RiskType
+from django.contrib.auth.models import User
+
+
+class RiskFactory(factory.DjangoModelFactory):
+    class Meta:
+        model = Risk
+
+    name = "vehicle"
+
+
+class UserFactory(factory.DjangoModelFactory):
+    class Meta:
+        model = User
+
+    username = "admin"
+
+
+class RiskTypeFactory(factory.DjangoModelFactory):
+    class Meta:
+        model = RiskType
+
+    name = "vehicle_risk_type"
+    owner = factory.SubFactory(UserFactory)
+
+
+class FieldTypeFactory(factory.DjangoModelFactory):
+    class Meta:
+        model = FieldType
+
+    name = "field_type"
+    risk_type = factory.SubFactory(RiskTypeFactory)
+
+
+class FieldOptionFactory(factory.DjangoModelFactory):
+    class Meta:
+        model = FieldOption
+
+    label = "label"
+    field_type = factory.SubFactory(FieldTypeFactory)
+    choice = ""
---
+++ bce-server/bce/tests/test_views.py
@@ -0,0 +1,1 @@
+# Insert View tests here
